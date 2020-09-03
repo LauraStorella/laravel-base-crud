036b3cc967c4107a6615c5372273fe29cf8dc8fe
Laura <lstorella10@gmail.com>
2020-09-03 17:26:39 +0200

    Destroy (eliminazione record)

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,6 +5,12 @@
   <li>Titolo: {{ $movie->title }} 
     <a href="{{ route('movies.show', $movie->id ) }}">Show details</a>
     <a href="{{ route('movies.edit', $movie->id ) }}">Edit</a>  
+
+    <form action="{{ route('movies.destroy', $movie->id ) }}" method="post">
+      @csrf
+      @method('DELETE')
+      <input type="submit" value="Delete">
+    </form>
   </li>
   @endforeach
 </ul>
